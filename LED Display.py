0a5543ea438fd2dda4a0e56413e2847853d8f7d8
Aut0Matic <82569441+Aut0Matic@users.noreply.github.com>
2022-01-12 19:04:09 +1030

    Update LED Display.py

--- LED Display.py
+++ LED Display.py
@@ -61,35 +61,10 @@ class visualHull(SampleBase):
             # <0: right turn
             value = (p1[0]-p2[0])*(p3[1]-p2[1])-(p1[1]-p2[1])*(p3[0]-p2[0])
             return value
-        """
-        # Determining the bottom point
-
-        bottom_point = bottom(points)
-
-        # Sorting all points by anticlockwise angle relative to the bottom point
-
-        points =sorted(points, key=lambda p: (math.atan2(p[1]-bottom_point[1], p[0]-bottom_point[0])))
-
-        # Defining the stack
-
-        stack = []
-
-        # Iterate through all points, add the next one
-        for i in points:
-            stack.append(i)
-            if len(stack)>2:
-                
-                # While the final 3 line segment turns right, remove the middle segment.
-                
-                while turn(stack[-3], stack[-2], stack[-1])>=0:
-                    
-                    temp = stack[-1] # Grabbing the end
-                    
-                    stack.pop() # Popping end
-                    stack.pop() # Popping middle
-                    
-                    stack.append(temp) # Replacing End             
-        """                 
+       
+       
+       #! Main Loop
+       
         while True:
             # Clearing the matrix
             self.matrix.Fill(0,0,0)
@@ -134,30 +109,33 @@ class visualHull(SampleBase):
                    
                 if len(stack)>2:
                    
-                   # While the final 3 line segment turns right, remove the middle segment. 
-                    while turn(stack[-3], stack[-2], stack[-1])>=0:
-                        
-                        # This is used to paint red
-                        not_in_hull.append(stack[-2])
-                        
-                        line(stack[-3], stack[-2], 255, 0, 0, 0.001)
-                        line(stack[-2], stack[-1], 255, 0, 0, 0.001)
-                        
-                        time.sleep(0.2) 
-                        
-                        temp = stack[-1] # Grabbing the end
-                        
-                        stack.pop() # Popping end
-                        stack.pop() # Popping middle
-                        
-                        stack.append(temp) # Replacing End
-                        
-                        line(stack[-2], not_in_hull[-1], 0, 0, 0, 0.001)
-                        line(not_in_hull[-1], stack[-1], 0, 0, 0, 0.001)
-                        
-                        draw_list(not_in_hull, 255, 0, 0, 0.001)
-                        line(stack[-2], stack[-1], 38, 255, 0, 0.25)
-            line(points[-1], points[1], 38, 255, 0, 0.25)
+                    # While the final 3 line segment turns right, remove the middle segment.
+                    try: 
+                        while turn(stack[-3], stack[-2], stack[-1])>=0:
+                            
+                            # This is used to paint red
+                            not_in_hull.append(stack[-2])
+                            
+                            line(stack[-3], stack[-2], 255, 0, 0, 0.001)
+                            line(stack[-2], stack[-1], 255, 0, 0, 0.001)
+                            
+                            time.sleep(0.2) 
+                            
+                            temp = stack[-1] # Grabbing the end
+                            
+                            stack.pop() # Popping end
+                            stack.pop() # Popping middle
+                            
+                            stack.append(temp) # Replacing End
+                            
+                            line(stack[-2], not_in_hull[-1], 0, 0, 0, 0.001)
+                            line(not_in_hull[-1], stack[-1], 0, 0, 0, 0.001)
+                            
+                            draw_list(not_in_hull, 255, 0, 0, 0.001)
+                            line(stack[-2], stack[-1], 38, 255, 0, 0.25)
+                    except:
+                        print("funky error")
+            line(points[-1], points[0], 38, 255, 0, 0.25)
                 
             time.sleep(0.25)
 # Main function
@@ -165,67 +143,3 @@ if __name__ == "__main__":
     visual_hull = visualHull()
     if (not visual_hull.process()):
         visual_hull.print_help()
-
-
-
-
-
-
-""" points = []
-
-for i in range(1,50):
-    Px = random.randrange(-25,25)
-    Py = random.randrange(-25,25)
-    
-    points.append((Px, Py))
-    
-# Function to solve the lowest y-value point
-
-def bottom(points):
-    minimum = (11, 11)
-    for i in points:
-        if i[1]<minimum[1]:
-            minimum = i
-    return minimum
-
-# Function to solve which direction the line connected by p1-p2-p3
-
-def turn(p1, p2, p3):
-    # = 0: collinear
-    # >0: left turn
-    # <0: right turn
-    value = (p1[0]-p2[0])*(p3[1]-p2[1])-(p1[1]-p2[1])*(p3[0]-p2[0])
-    return value
-
-# Determining the bottom point
-
-bottom_point = bottom(points)
-
-# Sorting all points by anticlockwise angle relative to the bottom point
-
-points =sorted(points, key=lambda p: (math.atan2(p[1]-bottom_point[1], p[0]-bottom_point[0])))
-
-# Defining the stack
-
-stack = []
-
-# Iterate through all points, add the next one
-for i in points:
-    stack.append(i)
-    if len(stack)>2:
-        
-        # While the final 3 line segment turns right, remove the middle segment.
-        
-        while turn(stack[-3], stack[-2], stack[-1])>=0:
-            
-            temp = stack[-1] # Grabbing the end
-            
-            stack.pop() # Popping end
-            stack.pop() # Popping middle
-            
-            stack.append(temp) # Replacing End
-                        
-print("All points: ", points)
-
-print("Hull points: ", stack)
- """
