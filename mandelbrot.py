8ba7e84d5c8ba711765f09c020e4acbbebfe714b
Austen Lindsay <aalindsay@me.com>
2022-03-16 22:15:13 +1030

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -73,25 +73,23 @@ class TestBench(SampleBase):
         while True:
             
             for var in range(1,10):  
-                print("var = ", var);
                 time.sleep(0.00001)
                 
                 for i in range(0,192):
-                    print("i = ", i)
                     time.sleep(0.00001)
                     
                     for y in range(0, 64):
-                        print("y = ", y)
                         time.sleep(0.00001)
                         
                         z = base_array[i][y]
 
                         result = escape(z, var)
+                        print(result)
                         
                         sc = int((1/var) * 255)
                         
                         if result == var:
-                            new_pixel(i, y, 10, 10, sc)
+                            new_pixel(i, y, sc, sc, 10)
 
                         
                     
